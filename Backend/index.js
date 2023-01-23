33a8fca0c962344f89890746c16a5fbd92014b0a
habibulmursaleen <habibulmursaleen97@gmail.com>
2023-01-23 09:57:06 +0100

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser"); //Express middleware
-const axios = require("axios"); //For fetch API data
-const Pool = require("pg").Pool; //For postgres connection
 const app = express(); //Express.js framework
 const port = 8000; //Backend port
 const cors = require("cors"); //to connect with frontend
-const dotenv = require("dotenv"); //For .env
-dotenv.config();
 const router = require("./routes/dataRoutes");
 
 //Parsing JSON and Raw data
