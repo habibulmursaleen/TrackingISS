dc16cb7f2c04abaf993ccc7860a6334a4f3a5b93
habibulmursaleen <habibulmursaleen97@gmail.com>
2023-02-14 08:39:21 +0100

    commit

--- Client/src/components/Loading.jsx
+++ Client/src/components/Loading.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import classes from "../Styles/Styles.module.css";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className={classes.mapContainer}>
+      <h1>
+        Loading the position data. Google Map will be visible soon. Please
+        wait..
+      </h1>
+    </div>
+  );
 };
 
 export default Loading;
--- Client/src/components/Maps.jsx
+++ Client/src/components/Maps.jsx
@@ -1,7 +1,7 @@
 import { GoogleMap, Marker, Polyline } from "@react-google-maps/api";
 import React from "react";
 
-//interner imports
+//internal imports
 import classes from "../Styles/Styles.module.css";
 import marker from "../assets/marker.png";
 import { useStateContext } from "../context/ContextProvider";
@@ -18,57 +18,60 @@ const Maps = () => {
   const { positions, setSelected, selected } = useStateContext();
 
   return (
-    <div className={classes.mapContainer}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        //will center the map according to last position
-        center={{
-          lat: parseFloat(positions[positions.length - 1].lat),
-          lng: parseFloat(positions[positions.length - 1].lng),
-        }}
-        zoom={5}
-      >
-        {/* For continuous line */}
-        <Polyline
-          path={[
-            // origin - first element of the useState hook (positions array)
-            {
-              lat: parseFloat(positions[0].lat),
-              lng: parseFloat(positions[0].lng),
-            },
-            // destination - last element of the useState hook (positions array)
-            {
-              lat: parseFloat(positions[positions.length - 1].lat),
-              lng: parseFloat(positions[positions.length - 1].lng),
-            },
-          ]}
-          options={{
-            strokeColor: "#FFF",
-            strokeOpacity: 1,
-            strokeWeight: 3,
-            geodesic: true,
-            clickable: false,
-          }}
-        />
-
-        {/* for markers */}
-        <Marker
-          position={{
+    <div className={classes.Container}>
+      <h1>Welcome to ISS tracking Application</h1>
+      <div className={classes.mapContainer}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          //will center the map according to last position
+          center={{
             lat: parseFloat(positions[positions.length - 1].lat),
             lng: parseFloat(positions[positions.length - 1].lng),
           }}
-          onClick={() => {
-            setSelected(positions[positions.length - 1]);
-          }}
-          icon={{
-            url: marker, //getting a custom marker for iss
-            scaledSize: new window.google.maps.Size(40, 40),
-          }}
-        />
+          zoom={5}
+        >
+          {/* For continuous line */}
+          <Polyline
+            path={[
+              // origin - first element of the useState hook (positions array)
+              {
+                lat: parseFloat(positions[0].lat),
+                lng: parseFloat(positions[0].lng),
+              },
+              // destination - last element of the useState hook (positions array)
+              {
+                lat: parseFloat(positions[positions.length - 1].lat),
+                lng: parseFloat(positions[positions.length - 1].lng),
+              },
+            ]}
+            options={{
+              strokeColor: "#FFF",
+              strokeOpacity: 1,
+              strokeWeight: 3,
+              geodesic: true,
+              clickable: false,
+            }}
+          />
 
-        {/* if selected then the infoWindow will show */}
-        {selected && <InfoWindows />}
-      </GoogleMap>
+          {/* for markers */}
+          <Marker
+            position={{
+              lat: parseFloat(positions[positions.length - 1].lat),
+              lng: parseFloat(positions[positions.length - 1].lng),
+            }}
+            onClick={() => {
+              setSelected(positions[positions.length - 1]);
+            }}
+            icon={{
+              url: marker, //getting a custom marker for iss
+              scaledSize: new window.google.maps.Size(40, 40),
+            }}
+          />
+
+          {/* if selected then the infoWindow will show */}
+          {selected && <InfoWindows />}
+        </GoogleMap>
+      </div>
     </div>
   );
 };
